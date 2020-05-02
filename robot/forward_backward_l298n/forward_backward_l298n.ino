f379a6ea6d5d47f81cdab58629be7932c6723c18
david-riser <dmriser@gmail.com>
2020-05-02 08:54:12 -0500

    Added the thing

--- robot/forward_backward_l298n/forward_backward_l298n.ino
+++ robot/forward_backward_l298n/forward_backward_l298n.ino
@@ -9,28 +9,35 @@
 const int enA = 9;
 const int in1 = 6; 
 const int in2 = 7;
-const int motorLow = 50;
-const int motorHigh = 120;
+const int motorLow = 30;
+const int motorHigh = 80;
 
 void setup() {
   pinMode(enA, OUTPUT);
   pinMode(in1, OUTPUT);
   pinMode(in2, OUTPUT);
 
+  // Allow a slight delay upon start.
+  delay(1000);
+
   // Initialize the direction
   digitalWrite(in1, LOW);
   digitalWrite(in2, HIGH);
+
+  // PWM to L298N (0-255)
+  analogWrite(enA, motorLow);
 }
 
 void loop() {
 
-  // PWM to L298N (0-255)
-  analogWrite(enA, motorLow);
-  
+  // Initialize the direction
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, HIGH);
+  delay(5000);
+
   // We are going forward/backward for 5 seconds
   // then we change directions.
-  delay(5000);
   digitalWrite(in1, HIGH);
   digitalWrite(in2, LOW);
-  
+  delay(5000);    
 }
