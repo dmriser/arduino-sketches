481afd6fd64a174fe3925f7535d7b029733d8375
david-riser <dmriser@gmail.com>
2020-05-06 15:54:46 -0500

    Adding filter

---
+++ robot/hc_sr04_filter/hc_sr04_filter.ino
@@ -0,0 +1,24 @@
+/* 
+ *  Sketch to test the HC-SR04 sonar sensor.  This
+ *  time do some filtering. 
+ */
+
+#include <NewPing.h>
+
+enum pins {
+  trigger = 9,
+  echo = 10
+};
+
+const int MAX_DIST = 400; 
+
+NewPing sonar(pins::trigger, pins::echo, MAX_DIST);
+
+void setup(){
+  Serial.begin(9600);
+}
+
+void loop(){
+  //delay(50);
+  Serial.println(sonar.ping_median(8));
+}
